b0e7b304d0b3f4bcfd78f2e5fab9d68ebfeeb534
arvind singh <rathourarv@gmail.com>
2020-10-07 18:50:42 +0530

    Update start method to roll dice configured number of times

--- src/game.js
+++ src/game.js
@@ -10,7 +10,12 @@ class Game {
   }
 
   start() {
-    console.log("starting...")
+    const name = prompt('Please enter your name: ');
+    console.log(`welcome to Snakes and Ladders ${name}`)
+    for (let turn = 0; turn < this.numberOfSteps; turn++) {
+      const value = this.dice.roll()
+      console.log(`Your turn number: ${turn} and dice value is ${value}`)
+    }
   }
 }
 
